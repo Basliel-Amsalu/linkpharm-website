63fd00f5d3fe9a57ecfd546eca6c6735caa4bdad
Basliel-Amsalu <acychappiekiller@gmail.com>
2024-03-30 15:55:38 +0300

    adding a footer and some more edits

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -18,6 +18,7 @@ export default {
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+        display: ["group-hover"],
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
